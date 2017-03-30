e51d3dc29dd483bce7a667ed2a26eeebf59c75c2
Matthew Milstead <mattgmilstead@gmail.com>
2017-03-30 11:50:16 -0400

    Adding Structs, Lists, LINQ

---
+++ 0.15_Structs/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._15_Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBookEntry Paul = new AddressBookEntry()
+            {
+                Address = "123 Coder pl",
+                City = "Westfield",
+                State = "IN",
+                Zip = "44056",
+                FirstName = "Paul",
+                LastName = "O'Conner",
+                Age = 40
+
+
+            };
+
+            AddressBookEntry Tommy = new AddressBookEntry()
+            {
+                Address = "Meridian st",
+                City = "Indidanpolis",
+                State = "IN",
+                Zip = "46217",
+                FirstName = "Tommy",
+                LastName = "Hurly",
+                Age = 21
+
+
+            };
+
+            AddressBookEntry Billy = new AddressBookEntry()
+            {
+                Address = "Washington st",
+                City = "Westfield",
+                State = "IN",
+                Zip = "44105",
+                FirstName = "Billy",
+                LastName = "Hurly",
+                Age = 23
+
+
+            };
+
+            AddressBookEntry Bre = new AddressBookEntry()
+            {
+                Address = "123 Coder pl",
+                City = "Mcaster",
+                State = "IN",
+                Zip = "4305",
+                FirstName = "Breeanna",
+                LastName = "Elliott",
+                Age = 21
+
+
+            };
+
+            Console.WriteLine(Paul.GetAddress());
+            Console.WriteLine("");
+            Console.WriteLine(Billy.GetAddress());
+            Console.WriteLine("");
+            Console.WriteLine(Tommy.GetAddress());
+            Console.WriteLine("");
+            Console.WriteLine(Bre.GetAddress());
+            Console.ReadLine();
+        }
+
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Program (string firstName,string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        struct AddressBookEntry
+        {
+            public string FirstName;
+            public string LastName;
+            public int Age;
+            public string Address;
+            public string City;
+            public string State;
+            public string Zip;
+
+            public string GetAddress()
+            {
+                return $"Address: {Address}\nCity: {City}\nState: {State}\nZip: {Zip}\n";
+            }
+        }
+    }
+}
---
+++ 0.17_LINQ/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._17_LINQ                   //Laungue Intagretated N#### Querry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<UsedCar> usedCars = new List<UsedCar>
+            {
+                new UsedCar(){VIN = "A1", Make = "Hyundai", Model = "Elantra", Price = 5000, Year = 2009},
+                new UsedCar(){VIN = "B1", Make = "Hyundai", Model = "Elantra", Price = 3000, Year = 2004},
+                new UsedCar(){VIN = "C1", Make = "Ford", Model = "Tarus", Price = 7000, Year = 2010},
+                new UsedCar(){VIN = "D1", Make = "Ford", Model = "Escape", Price = 15000, Year = 20014},
+                new UsedCar(){VIN = "E1", Make = "BMW", Model = "55i", Price = 9000, Year = 2015},
+                new UsedCar(){VIN = "F1", Make = "BMW", Model = "55i", Price = 6500, Year = 2002},
+                new UsedCar(){VIN = "G1", Make = "Toyota", Model = "4Runner", Price = 8700, Year = 2011},
+            };
+
+            var bmws = from car in usedCars
+                      where car.Make == "BMW"
+                      select car;
+            foreach (var bmw in bmws)
+            {
+                Console.WriteLine("The {0} {1} is a nice car that costs: ${2}", bmw.Year, bmw.Make, bmw.Price);
+            }
+            Console.WriteLine("============");
+
+
+
+            var cars = from car in usedCars
+                       where car.Price <= 10000 //&& car.Price > 5000 //&& car.Make == "BMW"
+                       select car;
+            foreach (var car in cars)
+            {                                                                 //{2:C}  :C = "$#"
+                Console.WriteLine("The {0} {1} costs less $10,000 at a cost of: {2:C}", car.Year, car.Make, car.Price);
+            }
+            Console.WriteLine("============");
+            Console.WriteLine("");
+
+            Console.WriteLine("LINQ with Lambda");
+            var toyotas = usedCars.Where(car => car.Make == "Toyota");          //Condensed
+            
+                foreach (var toyota in toyotas)
+            {
+                Console.WriteLine(toyota.Model);
+            }
+
+
+            var niceUsedCars = usedCars.Where(car => car.Price > 7500 && car.Make == "BMW" && car.Make == "Ford");
+            foreach (var niceUsedCar in niceUsedCars)
+            {
+                Console.WriteLine(niceUsedCar.Model);
+            }
+
+
+            Console.WriteLine("============");
+            Console.ReadLine();
+            Console.WriteLine("============");
+
+
+
+        }
+    }
+
+    class UsedCar
+    {
+        public string VIN { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double Price { get; set; }
+    }
+
+
+
+}
