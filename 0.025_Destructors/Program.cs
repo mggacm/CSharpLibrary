98d79e23cd5d22fff41825bcfb4ad7bcd186262e
Matthew Milstead <mattgmilstead@gmail.com>
2017-05-24 11:49:04 -0400

    Destructors

---
+++ 0.025_Destructors/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._025_Destructors
+{
+    class first
+    {
+        public first()
+        {
+            Console.WriteLine("First Object Created. Press Enter to destroy it.");
+        }
+        ~first()
+        {
+            Console.WriteLine("Destroying First Object");
+            Console.ReadLine();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            first ft = new first();
+            Console.ReadLine();
+        }
+    }
+
+}
+
