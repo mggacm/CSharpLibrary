32c291613fe27ac2c670af8d72212699498df8af
Matthew Milstead <mattgmilstead@gmail.com>
2017-05-26 09:41:10 -0400

    Adding fizzbuzz

---
+++ 0.25_Trim/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._25_Trim
+{    
+    class Program
+    {
+        static void Main()
+        {
+            // Input string.
+            string st = "  This is an example string. ";
+
+            // Call Trim instance method.
+            // This returns a new string copy.
+            st = st.Trim();
+
+            Console.WriteLine(st);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        public void DoFizzBuzz()
+        {
+            for (int i = 1; i < 101; i++) Console.WriteLine("{0:#;}{1:;;Fizz}{2:;;Buzz}", i % 3 * i % 5 == 0 ? 0 : i, i % 3, i % 5);
+        }
+
+        static void Main(string[] args)
+        {
+            Program few = new Program();
+            few.DoFizzBuzz();
+
+            //for (int n = 1; n <= 100; n++)
+            //{
+            //    if (n % 15 == 0)
+            //    {
+            //        Console.WriteLine("FizzBuzz");
+            //    }
+            //    else if (n % 3 == 0)
+            //    {
+            //        Console.WriteLine("Fizz");
+            //    }
+            //    else if (n % 5 == 0)
+            //    {
+            //        Console.WriteLine("Buzz");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(n);
+            //    }
+            //}
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
