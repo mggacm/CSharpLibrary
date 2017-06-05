d29e5373e69f5df5ce0e7f605486bd1f925e3053
Matthew Milstead <mattgmilstead@gmail.com>
2017-06-05 13:44:12 -0400

    MoreLINQ

---
+++ 0.17.5_MoreLINQ/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _0._17._5_MoreLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] dirfiles = Directory.GetFiles("c:\\sri\\");
+            var avg = dirfiles.Select(file => new FileInfo(file).Length).Average();
+            avg = Math.Round(avg / 10, 1);
+            Console.WriteLine("The Average file size is {0} MB", avg);
+            Console.ReadLine();
+        }
+    }
+}
